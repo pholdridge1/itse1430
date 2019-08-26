d53679b731e9b9f93808be7de3e563204c134c4c
philip.oldridge <philip.oldridge@NEITD-211209>
2019-08-26 18:56:12 -0500

    Talked about variables and types.

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -6,7 +6,25 @@ namespace HelloWorld
     {
         static void Main ( string[] args )
         {
+            string name = "";
+
+            // Definitely assigned
+            // name = "Bob";
+            string name2 = name;
+            name2 = Console.ReadLine ();
+            Console.WriteLine (name2);
+
             Console.WriteLine ("Hello World!");
+
+            // Another block
+            // Yet another block
+
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = payRate * hours;
+
+            string fullName = "Fred " + " " + "Jones";
         }
     }
 }
