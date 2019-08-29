159be5c1f3c367760aeb6f6f67fe731a92f0e372
philip.oldridge <philip.oldridge@NEITD-211209>
2019-08-28 19:12:59 -0500

    ReadInt32, ReadBoolean, DisplayMenu created.

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -6,6 +6,102 @@ namespace HelloWorld
     {
         static void Main ( string[] args )
         {
+            // Movie data
+            string title;
+            int runLength;
+            int releaseYear;
+            string description;
+            bool haveSeen;
+
+            while(true)
+            {
+                char input = DisplayMenu();
+                if (input == 'A')
+                    AddMovie ();
+                else if (input == 'Q')
+                    break;
+            };
+            
+        }
+
+        static void AddMovie()
+        {
+            // Get title
+            Console.Write("Title: ");
+            string title = Console.ReadLine ();
+
+            // Get description
+            Console.Write ("Description: ");
+            string description = Console.ReadLine ();
+
+            // Get release year
+            int releaseYear = ReadInt32("Release Year: ");
+
+            // Get run length
+            int runLength = ReadInt32("Run Length (in minutes): ");
+
+            // Get have seen
+            bool haveSeen = ReadBoolean("Have Seen? ");
+        }
+
+        static int ReadInt32 (string message)
+        {
+            while (true)
+            {
+                Console.Write (message);
+
+                string input = Console.ReadLine ();
+
+                // int result = Int32.Parse (input);
+                if (Int32.TryParse (input, out int result))
+                    return result;
+
+                Console.WriteLine ("Not a number");
+            };
+        }
+
+        static bool ReadBoolean( string message )
+        {
+            while (true)
+            {
+                Console.Write (message);
+
+                string input = Console.ReadLine ();
+
+                // int result = Int32.Parse (input);
+                bool result;
+                if (Boolean.TryParse (input, out result))
+                    return result;
+
+                Console.WriteLine ("Not a boolean");
+            };
+        }
+
+        static char DisplayMenu()
+        {
+            do
+            {
+                Console.WriteLine("(A)dd Movie");
+                Console.WriteLine("(Q)uit");
+
+                string input = Console.ReadLine ();
+                if (input == "A" || input == "a")
+                {
+                    return 'A';
+                }
+                else if (input == "Q" || input == "q")
+                {
+                    return 'Q';
+                }
+                else
+                {
+                    Console.WriteLine ("Invalid input");
+                }
+
+            } while(true);
+        }
+        private static void DemoLanguage ()
+        {
             string name = "";
 
             // Definitely assigned
