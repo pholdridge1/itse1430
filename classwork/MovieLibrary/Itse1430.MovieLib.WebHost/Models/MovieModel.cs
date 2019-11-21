68bbb2f321425116466361c0bd7cd11fbeeb88c9
philip.oldridge <philip.oldridge@NEITD-211209>
2019-11-20 18:31:48 -0600

    WebApp stuff

---
+++ classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Models/MovieModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Itse1430.MovieLib.WebHost.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+
+        [RequiredAttribute(AllowEmptyStrings = false)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Rating { get; set; }
+
+        [Display(Name = "Release Year")]
+        [Range(1900, Int32.MaxValue, ErrorMessage = "Release year must be >= 1900")]
+        public int ReleaseYear { get; set; } = 1900;
+
+        [RangeAttribute(0, Int32.MaxValue, ErrorMessage = "Run length must be >= 0")]
+        public int RunLength { get; set; }
+
+        public bool HaveSeen { get; set; }
+    }
+}
---
+++ classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Models/MovieModelExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itse1430.MovieLib.WebHost.Models
+{
+    public static class MovieModelExtensions
+    {
+        public static MovieModel ToModel ( this Movie source)
+        {
+            if (source == null)
+                return null;
+
+            return new MovieModel() {
+                Id = source.Id,
+                Title = source.Title,
+                Description = source.Description,
+                HaveSeen = source.HaveSeen,
+                Rating = source.Rating,
+                ReleaseYear = source.ReleaseYear,
+                RunLength = source.RunLength
+            };
+        }
+    }
+}
